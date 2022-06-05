c26e611f0dae78c991a41663718063d6f3b75dac
Farjana Begum <fjkarim1971@gmail.com>
2022-06-04 23:21:58 -0700

    Variable declaration

---
+++ Fixed_Input.java
@@ -0,0 +1,17 @@
+package Java_Basic;
+
+public class Fixed_Input {
+
+	public static void main(String[] args) {
+
+
+		// data type  variable name = value (variable declaration)
+		
+		int number = 11;
+	       String name = "Java";
+	       
+	       System.out.println("Number :" +number);
+	       System.out.println("Name :" +name);
+	}
+
+}
