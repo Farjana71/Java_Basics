1810b1b1a7258c6b83f5d3da49fe8a61f60a5fef
Farjana Begum <fjkarim1971@gmail.com>
2022-06-05 19:32:49 -0700

    Take Input from User

--- User_Input.java
+++ User_Input.java
@@ -1,9 +1,23 @@
 package Java_Basic;
 
+import java.util.Scanner;
+
 public class User_Input {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		
+		//DataType VariableName = Value (Variable declaration)
+		Scanner input = new Scanner (System.in);
+		System.out.print("Please Enter a Number :");  
+		int number = input.nextInt() ;
+		
+		System.out.print("Please Enter a Name :");
+	      String name = input.next();
+	       
+	       System.out.println("Number :" +number);
+	       System.out.println("Name :" +name);
+		
 
 	}
 
